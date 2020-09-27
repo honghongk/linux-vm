d191c39d6bf05a4bc26e3fc84579ae1c6625c760
root <root@msec.asia-east2-a.c.msecret.internal>
2020-09-27 12:47:01 +0000

    centos 8

--- init.sh
+++ init.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-yum=`which yum`
-${yum} install git nginx php php-mysqlnd php-fpm php-json php-pdo -y
+
+dnf install epel-release -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module reset php -y
+dnf module enable php:remi-7.4 -y
+dnf install git nginx php php-mysqlnd php-fpm php-json php-pdo -y
+
+dnf install php-pecl-mongodb php-pecl-redis -y
+
+
+
+
+
+
+
+#yum=`which yum`
+
+#${yum} install epel-release -y
+#${yum} install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+#${yum} install git nginx php php-mysqlnd php-fpm php-json php-pdo -y
 
 
 
